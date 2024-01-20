848d2e1fc54ff3651e996a513234bc5798074b81
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-20 14:54:36 +0200

    added BrowserRouter and QueryClientProvider to use react-router-dom and @tanstack-query

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js"
 
@@ -11,10 +12,13 @@ const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
+  <BrowserRouter>
+  
+    <QueryClientProvider client={queryClient}>
 
-    <App />
-  </QueryClientProvider>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
