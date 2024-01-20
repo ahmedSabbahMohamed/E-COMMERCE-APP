138562a217bd6fa95485bca3dc110803c51e9ea6
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-20 14:53:20 +0200

    added Routes and Header, Signin, and Register components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Signin from "./components/Signin";
+import Register from "./components/Register";
+
 function App() {
   return (
-    <h1 className="text-primary">Hello, world!</h1>
+    <>   
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Signin />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
