f5471e74db8dbbc616b000604b348b03b84609f2
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-24 13:57:50 +0200

    imported FormContainer styles

--- src/features/authentication/components/FormContainer.js
+++ src/features/authentication/components/FormContainer.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import formImg from "../assets/images/formImg.jpg"
+import "../assets/styles/FormContainer.css"
 
 function FormContainer({form, authType}) {
   return (
-    <Container className="mt-5 mb-3">
+    <Container className="form-container mt-5">
       <Row className="rounded mx-2 shadow overflow-hidden">
-        <Col className="p-0" sm={12} md={4}>
-          <img className="w-100 h-100" src={formImg} alt="form" />
-        </Col>
+        <Col className="p-0 form-img" sm={12} md={4}></Col>
         <Col className="py-4" sm={12} md={8}>
           <Row className="">
             <Col>
