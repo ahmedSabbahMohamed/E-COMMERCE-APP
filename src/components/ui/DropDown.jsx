3b93c27f9538371ef10f46a8b6efad8b2d0b38bc
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-24 13:54:53 +0200

    modified links and style

--- src/components/ui/DropDown.jsx
+++ src/components/ui/DropDown.jsx
@@ -1,28 +1,29 @@
+import { Link } from "react-router-dom";
+import links from "../../data/navData.json"
+
 function DropDown() {
   return (
     <div className="dropdown">
-      <a
-        className="btn text-black dropdown-toggle"
-        href="#"
-        role="button"
+      <button
+        className="btn border-0 text-white fw-bold dropdown-toggle"
         id="dropdownMenuLink"
         data-bs-toggle="dropdown"
         aria-haspopup="true"
         aria-expanded="false"
       >
         Categories
-      </a>
+      </button>
 
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-        <a className="dropdown-item" href="#">
-          one
-        </a>
-        <a className="dropdown-item" href="#">
-          two
-        </a>
-        <a className="dropdown-item" href="#">
-          three
-        </a>
+      <div className="dropdown-menu shadow border-0" aria-labelledby="dropdownMenuLink">
+        {
+          links.categories.map(category => {
+            return (
+              <Link className="dropdown-item" to={category.href} key={category.id}>
+                {category.link}
+              </Link>
+            )
+          })
+        }
       </div>
     </div>
   );
