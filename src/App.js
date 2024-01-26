4c33d8036ed7d456e156666c1612c72b79903d83
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-26 14:54:51 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login"
 import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </>
   );
 }
