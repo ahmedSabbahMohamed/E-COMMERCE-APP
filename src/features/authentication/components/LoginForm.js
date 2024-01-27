caebc5ec431849ba468f9346fb16944c9db9e1e3
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-01-27 21:17:23 +0200

    added fomrikProps to the Form

--- src/features/authentication/components/LoginForm.js
+++ src/features/authentication/components/LoginForm.js
@@ -1,58 +1,73 @@
 import { Form, Formik } from "formik"
-import FormContainer from "./FormContainer"
 import { Link } from "react-router-dom"
 import * as Yup from "yup"
+import { Container, Row, Col } from "react-bootstrap"
 import Input from "../../../components/form/Input"
 import SubmitBtn from "./SubmitBtn"
 
 function LoginForm() {
 
   const validationSchema = Yup.object({
-    email: Yup.string().required("email is required").email("invalid email address"),
-    password: Yup.string().required("password is required")
-  })
-
-  const initialValues = {
-    email: '',
-    password: ''
-  }
+    email: Yup.string()
+      .email("invalid email address")
+      .required("email is required"),
+    password: Yup.string().required("password is required"),
+  });
 
   const handleSubmit = (values) => {
     console.log(values)
   }
 
   return (
-    <FormContainer
-      form={
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form className="mx-auto d-grid gap-2">
-            <Input
-              label={"Email:"}
-              name={"email"}
-              type={"email"}
-              id={"email"}
-            />
+    <div className="form-container d-flex align-items-center justify-content-center">
+      <Container className="">
+        <Row className="rounded mx-2 shadow overflow-hidden">
+          <Col className="p-0 form-img" sm={12} md={4}></Col>
+          <Col className="py-4" sm={12} md={8}>
+            <Row className="">
+              <Col>
+                <h2 className="pb-3 text-center">Login</h2>
+                <Formik
+                  initialValues={{}}
+                  validationSchema={validationSchema}
+                  validateOnBlur
+                  validateOnChange
+                  onSubmit={handleSubmit}
+                >
+                  {(formikProps) => (
+                    <Form className="mx-auto d-grid gap-2">
+                      <Input
+                        label={"Email:"}
+                        name={"email"}
+                        type={"email"}
+                        id={"email"}
+                        onChange={formikProps.handleChange}
+                        onBlur={formikProps.handleBlur}
+                      />
+                      {/* {console.log(formikProps)} */}
+                      <Input
+                        label={"Password:"}
+                        name={"password"}
+                        type={"password"}
+                        id={"password"}
+                        onChange={formikProps.handleChange}
+                        onBlur={formikProps.handleBlur}
+                      />
 
-            <Input
-              label={"Password:"}
-              name={"password"}
-              type={"password"}
-              id={"password"}
-            />
-
-            <SubmitBtn btnTxt={"Login"} />
-            <p className="text-center">
-              don't have an account? <Link to={"/signup"}>Signup</Link>
-            </p>
-          </Form>
-        </Formik>
-      }
-      authType={"Login"}
-    />
+                      <SubmitBtn btnTxt={"Login"} />
+                      <p className="text-center">
+                        don't have an account?{" "}
+                        <Link to={"/signup"}>Signup</Link>
+                      </p>
+                    </Form>
+                  )}
+                </Formik>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
