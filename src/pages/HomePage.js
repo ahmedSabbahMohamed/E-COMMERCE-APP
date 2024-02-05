d4eb5e8da3ad1adc631cb90309d82b3ecb233ac7
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-02-05 12:59:35 +0200

    created HomePage page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Categories } from "../features/productCatalog"
+
+function HomePage() {
+  return (
+    <div className="container">
+        <Categories />
+    </div>
+  )
+}
+
+export default HomePage
